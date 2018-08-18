376ecb38786bc712a7772b5bb7c60ee9df63cec0
wilvillamizar <w_villamizarv@hotmail.com>
2018-08-18 17:21:56 -0500

    quinto commit

--- api_client.rb
+++ api_client.rb
@@ -1,12 +1,24 @@
 
-puts "ingrese su nombre"
-name = gets.chomp
+#puts "ingrese su nombre"
+puts "ingrese cuerpo publica"
+Post = gets.chomp
 
 require 'faraday'
 
 client = Faraday.new(url:'http://localhost:3000') do |config|
     config.adapter Faraday.default_adapter
+    config.token_auth('27ad8a21fb3a2e930d81465e21b9cef1')
 end
+
+
+response =client.post do |req|
+
+    req.url '/api/v1/posts'
+    req.headers['Content-Type']='application/json'
+    req.body ='{"post":{"body": "'+Post+'"}}'
+
+end
+=begin 
 response =client.post do |req|
 
     req.url '/api/v1/users'
@@ -14,7 +26,11 @@ response =client.post do |req|
     req.body ='{"user":{"name": "'+name+'"}}'
 
 end
+=end
 require 'oj'
 puts Oj.load(response.body)
 puts response.status
 
+#
+#"27ad8a21fb3a2e930d81465e21b9cef1"}
+#
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,36 @@
 module Api
     module V1
         class PostsController < ApplicationController
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    before_action :authenticate, only:[:create,:destroy]
             def index
                 @posts= Post.order('created_at DESC')
                 render json: @posts
             end
-            
+
+        def create
+            @post = @user.post.new(post_params)
+            if @post.save
+                render json: @post, status: :created
+            else
+                render json: @post.errors, status:  :unproccessable_entity 
+            end
+        end
+        private 
+        def authenticate
+        authenticate_or_request_with_http_token do |token,options|
+        @user = User.find_by(token: token)
+        
+
+        end
+    end
+
+
+        private 
+        def post_params
+            params.require(:post).permit(:body)
+
+        end 
         end
     end
 
